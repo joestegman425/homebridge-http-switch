d1fa540dec021971e2d00ee869d90340c80188e6
joestegman425 <mrjoestegman@gmail.com>
2025-10-23 12:13:15 -0700

    checkpoint update

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgePlaysoundWindowsPlugin';
+export const PLATFORM_NAME = 'HomebridgeHttpSwitch';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-playsound-windows';
+export const PLUGIN_NAME = 'homebridge-http-switch';
